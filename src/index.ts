7649b9d2f6f58ffbd05698d1a08c6452e34b7bd8
olegov-lab <olego8760@gmail.com>
2022-01-28 22:13:27 +0300

    feat: Webpack setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//import { App } from './app';
+import './normalize.css';
+import './index.css';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.body;
+  //const app = new App(rootElement);
+
+  //app.init();
+});
+
