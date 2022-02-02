69dbe5be8b7405ead50ebb13fc7a682069f33cf2
Pavel Chechelev <pashachechelev@gmail.com>
2022-02-02 23:37:58 +0300

    feat: delete audio-call description-layout

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { App } from './app';
 import './normalize.css';
 import './index.css';
 
-
 window.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.body;
   const app = new App(rootElement);
--- src/pages/games/games-selector/games-selector.ts
+++ src/pages/games/games-selector/games-selector.ts
@@ -1,5 +1,7 @@
-const body = document.body;
-body.insertAdjacentHTML('afterend', `
+import './games-selector.css';
+
+export const body = document.body;
+body.insertAdjacentHTML('afterbegin', `
 <section class="games-page" style="display: none;">
 <div class="container">
     <div class="games-page-title">Мини-игры</div>
