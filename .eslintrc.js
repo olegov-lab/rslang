214557ca9b4d7f7ea4a9b92358ed133f4d4d0753
Britva1910 <85491523+Britva1910@users.noreply.github.com>
2022-02-08 14:43:33 +0300

    feat: implement generating an array of questions for the game

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  },
+    extends: ["airbnb-base", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 13,
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
 
-  rules: {
-    "import/extensions": "off",
-    "import/prefer-default-export": "off",
-    "class-methods-use-this": "off",
-    "no-console": "off",
-    "default-param-last": "off",
-    "consistent-return": "off",
-  },
+    settings: {
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            },
+        },
+    },
+
+    rules: {
+        "import/extensions": "off",
+        "import/prefer-default-export": "off",
+        "class-methods-use-this": "off",
+        "no-console": "off",
+        "default-param-last": "off",
+        "consistent-return": "off",
+        "no-restricted-syntax": "off",
+    },
 };
--- src/pages/games/sprint/sprint-game/sprint.ts
+++ src/pages/games/sprint/sprint-game/sprint.ts
@@ -1,9 +1,46 @@
 import { getWords } from '../../../../api/api';
 
-const main = async () => {
-  const words = await getWords(0, 0);
-
-  console.log(words);
+const getWordsCollection = async (group = 1, page = 0) => {
+  const words = await getWords(group, page);
+  return words;
 };
 
-main();
+const generateWordsForGame = () => {
+  const wordsForGame = getWordsCollection().then((words) => {
+    const wordsCollection = [];
+    for (const variable in words) {
+      if ({}.hasOwnProperty.call(words, variable)) {
+        const currentIndex = Number(variable);
+        const randomBullean = Math.random() < 0.5;
+        if (randomBullean) {
+          wordsCollection.push({
+            word: words[currentIndex].word,
+            translate: words[currentIndex].wordTranslate,
+            answer: true,
+          });
+        } else {
+          const res = currentIndex < 19
+            ? wordsCollection.push({
+              word: words[currentIndex].word,
+              translate: words[currentIndex + 1].wordTranslate,
+              answer: false,
+            })
+            : wordsCollection.push({
+              word: words[currentIndex].word,
+              translate: words[currentIndex].wordTranslate,
+              answer: false,
+            });
+        }
+      }
+    }
+    return wordsCollection;
+  });
+  return wordsForGame; //  TODO сюда интерфейс
+};
+
+const render = async () => {
+  const res = await generateWordsForGame();
+  console.log(res);
+};
+
+render();
