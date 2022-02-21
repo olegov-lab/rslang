c0548134c08637cc3bd1b3168e25186cfad787f3
olegov-lab <olego8760@gmail.com>
2022-02-21 13:53:13 +0300

    feat:count learnword

--- src/pages/games/audio-call/audion-call-game/audio-call.ts
+++ src/pages/games/audio-call/audion-call-game/audio-call.ts
@@ -475,9 +475,9 @@ function getUzas() {
 
       let longestAnswerRightSprint = data.optional.longestAnswerRightSprint || +JSON.parse(localStorage.getItem('SprintStatistics'))?.longestAnswerRightSprint || 0;
 
-      let percentAnswerRightAudioCall = data.optional.percentAnswerRightAudioCall || +JSON.parse(localStorage.getItem('percentRightAudioCall'))?.percentAnswerRightAudioCall || 0;
+      let percentAnswerRightAudioCall = data.optional.percentAnswerRightAudioCall || +JSON.parse(localStorage.getItem('audioCallStatistics'))?.percentAnswerRightAudioCall || 0;
 
-      let LongestAnswerRightAudioCall = data.optional.LongestAnswerRightAudioCall || +JSON.parse(localStorage.getItem('LongestAnswerRightAudioCall'))?.LongestAnswerRightAudioCall || 0;
+      let LongestAnswerRightAudioCall = data.optional.LongestAnswerRightAudioCall || +JSON.parse(localStorage.getItem('audioCallStatistics'))?.LongestAnswerRightAudioCall || 0;
 
       let percentAnswerForDay: Number = +data.optional.percentAnswerForDay || (( percentAnswerRightSprint == 0) || (percentAnswerRightAudioCall == 0)) ? +percentAnswerRightSprint ||  +percentAnswerRightAudioCall : ((percentAnswerRightSprint + percentAnswerRightAudioCall) / 2) || 0;
 
@@ -551,8 +551,8 @@ function getUzas() {
               startDate: data.optional.startDate || startDate,
               percentAnswerRightSprint: JSON.parse(localStorage.getItem('SprintStatistics'))?.percentAnswerRightSpring || data.optional.percentAnswerRightSprint,
               longestAnswerRightSprint: +JSON.parse(localStorage.getItem('SprintStatistics'))?.longestAnswerRightSprint || data.optional.longestAnswerRightSprint,
-              percentAnswerRightAudioCall: +JSON.parse(localStorage.getItem('percentAnswerRightAudioCall'))?.percentAnswerRightAudioCall || data.optional.percentAnswerRightAudioCall,
-              LongestAnswerRightAudioCall: +JSON.parse(localStorage.getItem('LongestAnswerRightAudioCall'))?.LongestAnswerRightAudioCall || data.optional.LongestAnswerRightAudioCall,
+              percentAnswerRightAudioCall: +JSON.parse(localStorage.getItem('audioCallStatistics'))?.percentAnswerRightAudioCall || data.optional.percentAnswerRightAudioCall,
+              LongestAnswerRightAudioCall: +JSON.parse(localStorage.getItem('audioCallStatistics'))?.LongestAnswerRightAudioCall || data.optional.LongestAnswerRightAudioCall,
               percentAnswerForDay: percentAnswerForDay || data.optional.percentAnswerForDay,
               newWordAudioCallSum: 0,
               LearnWord: LearnWord.length,
@@ -615,8 +615,8 @@ function getUzas() {
               startDate: data.optional.startDate || startDate,
               percentAnswerRightSprint: JSON.parse(localStorage.getItem('SprintStatistics'))?.percentAnswerRightSpring || data.optional.percentAnswerRightSprint,
               longestAnswerRightSprint: +JSON.parse(localStorage.getItem('SprintStatistics'))?.longestAnswerRightSprint || data.optional.longestAnswerRightSprint,
-              percentAnswerRightAudioCall: +JSON.parse(localStorage.getItem('percentAnswerRightAudioCall'))?.percentAnswerRightAudioCall || data.optional.percentAnswerRightAudioCall,
-              LongestAnswerRightAudioCall: +JSON.parse(localStorage.getItem('LongestAnswerRightAudioCall'))?.LongestAnswerRightAudioCall || data.optional.LongestAnswerRightAudioCall,
+              percentAnswerRightAudioCall: +JSON.parse(localStorage.getItem('audioCallStatistics'))?.percentAnswerRightAudioCall || data.optional.percentAnswerRightAudioCall,
+              LongestAnswerRightAudioCall: +JSON.parse(localStorage.getItem('audioCallStatistics'))?.LongestAnswerRightAudioCall || data.optional.LongestAnswerRightAudioCall,
               percentAnswerForDay: percentAnswerForDay || data.optional.percentAnswerForDay,
               newWordAudioCallSum: 0,
               LearnWord: LearnWord.length,
